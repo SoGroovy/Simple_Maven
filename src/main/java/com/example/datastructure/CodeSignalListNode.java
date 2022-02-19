032b7f1ec908555efa756eca9202150399870356
Tin Phan <tinphan09@gmail.com>
2022-02-18 22:35:59 -0600

    CodeSignal List Nodes

--- src/main/java/com/example/datastructure/CodeSignalListNode.java
+++ src/main/java/com/example/datastructure/CodeSignalListNode.java
@@ -22,14 +22,32 @@ public class CodeSignalListNode {
     }
 
     public <T> ListNode<T> reverse(ListNode<T> list) {
-        List<T> l1 = new ArrayList<T>();
-        ListNode<T> reverseNode = new ListNode<T>(list.value);
+        ListNode<T> head = null;
+        ListNode<T> p = list;
         while (list != null) {
-            l1.set(0, list.value);
+            list = list.next;
+            p.next = head;
+            head = p;
+            p = list;
         }
-        for (int i = 0; i < l1.size(); i++) {
-            reverseNode.next = (ListNode<T>) l1.get(i);
-        }
-        return reverseNode.next;
+        return head;
     }
+
+    public ListNode<Integer> removeKFromList(ListNode<Integer> l, int k) {
+        ListNode<Integer> head = new ListNode<Integer>(0);
+        ListNode<Integer> current = head;
+        while (l != null) {
+            if (l.value != k) {
+                current.next = l;
+                current = current.next;
+            } else if (l.value == k && l.next == null) {
+                current.next = l.next;
+            }
+            l = l.next;
+        }
+
+        return head.next;
+    }
+
+
 }
